c2cc50869e177c0d4af41b214bc76eada84b012c
Dhrds <dhrds1996@gmail.com>
2023-04-02 21:45:03 -0300

    react 1.0.1

--- react/src/App.js
+++ react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import lin from './/in.png'
 import what from './/wh.png'
-import Teste from 'C:/Users/douglas/Desktop/git/my-app/src/teste.png'
+import Teste from './/teste.png'
 function App() {
   return (
     <div className="App">
@@ -13,27 +13,27 @@ function App() {
         </div>
       </header>
       <main>
-        <div className="container_0">
-          <div className="container_1">
-            <div className="canvas">
+        <section className="container_0">
+          <div className="container_1" />
+          <div className='container_2'>
+            <div className="canvas ">
               <img className="foto_1" src={Teste} alt="perfil" />
               <div className="tittle">
-                <h1 className="tittle">Douglas Silva</h1>
+                <h1 className="tittle1">Douglas Silva</h1>
                 <p className='subtitulo'>Desenvolvedor</p>
               </div>
               <footer children="canvas" className='footcanva'>
                 <a href='https://www.linkedin.com/in/douglas-silva-122aa01b6/'> <img src={lin} className='lin' /></a>
-               <a href='https://wa.me/5537999775765'> <img src={what} className='what' /></a>
-              </footer>         
+                <a href='https://wa.me/5537999775765'> <img src={what} className='what' /></a>
+              </footer>
             </div>
-            <div className='canvas_2'>
+            <div className='canvas_2 '>
               <span>Sobre mim</span>
             </div>
-
           </div>
-        </div>
+        </section>
       </main>
-     {/* <footer>
+      {/* <footer>
       </footer> */}
     </div>
   );
