5b9940dc87fafa58d85ca3e2f13da49fa5730582
Dhrds <dhrds1996@gmail.com>
2023-04-07 18:26:55 -0300

    .

--- lembrei/src/App.js
+++ lembrei/src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import { AiOutlineCheck } from 'react-icons/ai';
+import { useState} from 'react';
 
 function App() {
+
+  const [input,setInput] =useState ('')
+
+  function login(){
+    alert( `Seja bem vindo ${input}`)
+  }
+
   return (
     <div className="conteiner">
       <h1 className='title'>Lembrei</h1>
       <input className='user' type="text"
-        placeholder='usuario' />
+        placeholder='usuario'
+        value={input} 
+        onChange={(e)=>setInput(e.target.value)}/>
       
         <input className='senha' type="password"
           placeholder='senha' />
-        <button className='button-login'>
+        <button className='button-login' onClick={login}>
           <AiOutlineCheck size={25} color='#000'></AiOutlineCheck>
+          
         </button>
      
       <main>
