a53d2ffc016d2153524b51d775ad022a2208e578
Dhrds <dhrds1996@gmail.com>
2023-04-08 19:04:57 -0300

    whats data

--- api_python/whats.py
+++ api_python/whats.py
@@ -3,9 +3,26 @@ import pyautogui as a
 import keyboard as k
 import time
 import bd_lembrei as bd
-
+from datetime import date ,time as timehm
 
 while True:
-    pywhatkit.sendwhatmsg_instantly ("+5537999775765", "oi",10)
-    time.sleep(5)
-    k.press_and_release('enter')
+    bd.conexao
+    check = bd.select_bd.select('verificacao')
+    print(check)
+    if check == [('N',)]:
+        msg = bd.select_bd.select('parametros_mensagem')
+        print(msg)
+        ano =str(date.today.year)
+        mes = str(date.today.month)
+        dia = str(date.today.day)
+        hora = str(timehm.hour)        
+        minuto = str(timehm.minute)
+        data = (ano,mes,dia,hora,minuto)
+        print(data)
+        for i in msg :
+            if i[2] == data:
+                pywhatkit.sendwhatmsg_instantly ("+5537999775765", "oi",10)
+                time.sleep(5)
+                k.press_and_release('enter')
+    time.sleep (30)
+    
