29d1b5748f045d38d8c80e534eb921d24a3bd6b0
Dhrds <dhrds1996@gmail.com>
2023-04-22 21:16:59 -0300

    .

--- flask/main.py
+++ flask/main.py
@@ -1,8 +1,97 @@
-from flask import Flask, render_template, request,redirect ,flash
-import bd_lembrei as bd
+from flask import Flask, render_template, request, redirect, flash
+
 app = Flask(__name__)
-app.config['SECRET_KEY']="123"
+app.config['SECRET_KEY'] = "123"
 iduser = 0
+
+import mysql.connector
+from mysql.connector import Error
+
+
+class bd():
+    def select_usuario_login(email, senha):
+        connection = mysql.connector.connect(host="containers-us-west-87.railway.app:7991",
+                                             user="root",
+                                             password="aQG6PErjX4rFHnWY9Ocp",
+                                             db="railway")
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                cursor.execute("use railway")
+                cursor.execute(
+                    f"""select id from lembrei where email = '{email}' and senha = '{senha}'  """)
+                db_inf = cursor.fetchall()
+                print(db_inf)
+                return db_inf
+            else:
+                print('nao')
+        except Error as e:
+            print("Erro ao conectar ao MySQL", e)
+
+    def select_msg(id):
+        connection = mysql.connector.connect(host="containers-us-west-87.railway.app:7991",
+                                             user="root",
+                                             password="aQG6PErjX4rFHnWY9Ocp",
+                                             db="railway")
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                cursor.execute("use railway")
+                cursor.execute(
+                    f"""select p.data_hora,p.email,p.numero,p.mensagem_aparecer 
+                    from parametros_mensagem p
+                    where id_usuario = {id}  """)
+                db_inf = cursor.fetchall()
+                print(db_inf)
+                return db_inf
+            else:
+                print('nao')
+        except Error as e:
+            print("Erro ao conectar ao MySQL", e)
+
+    def insert_msg(lista_values):
+        connection = mysql.connector.connect(host="containers-us-west-87.railway.app:7991",
+                                             user="root",
+                                             password="aQG6PErjX4rFHnWY9Ocp",
+                                             db="railway")
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                cursor.execute("use railway")
+                print(
+                    f"""insert into parametros_mensagem (id_usuario,data_hora,email, numero,mensagem_aparecer) values {lista_values} ; """)
+                cursor.execute(
+                    f"""insert into parametros_mensagem
+                    (id_usuario,data_hora,email, numero,mensagem_aparecer)
+                    values {lista_values} ; """)
+                connection.commit()
+                cursor.close()
+                connection.close()
+                print('comitado')
+                return True
+        except Error as e:
+            print("usuario ou email ja usado", e)
+            return "usuario ou email ja usado"
+
+    def insert_usuario(lista_values):
+        connection = mysql.connector.connect(host="containers-us-west-87.railway.app:7991",
+                                                user="root",
+                                                password="aQG6PErjX4rFHnWY9Ocp",
+                                                db="railway")
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                cursor.execute("use railway")
+                cursor.execute(
+                    f"""insert into lembrei (numero,usuario,senha,email) values {lista_values}; """)
+                connection.commit()
+                cursor.close()
+                connection.close()
+                print('comitado')
+                return True
+        except Error as e:
+            print("usuario ou email ja usado", e)
+            return "usuario ou email ja usado"
 
 @app.route('/', methods=['POST', 'GET'])
 def login():
@@ -10,58 +99,62 @@ def login():
     senha = request.form.get('senha_cad')
     email = request.form.get('email_cad')
     numero = request.form.get('tel_cad')
-    cad =(numero,usuario,senha,email)
+    cad = (numero, usuario, senha, email)
     if cad != (None, None, None, None):
         check = bd.insert_usuario(cad)
         if check == True:
             return redirect('/home')
         else:
-            return render_template("index.html",msg = check)
-    return render_template("index.html")        
-    
-@app.route('/autentificar',methods=['POST', 'GET'])
+            return render_template("index.html", msg=check)
+    return render_template("index.html")
+
+
+@app.route('/autentificar', methods=['POST', 'GET'])
 def autent():
     email = request.form.get('email_login')
     senha = request.form.get('senha_login')
-    login = bd.select_usuario_login(email,senha)
+    login = bd.select_usuario_login(email, senha)
     if login != []:
-        global iduser 
+        global iduser
         iduser = login[0][0]
         return redirect('/home')
     else:
         return redirect('/#paralogin')
 
+
 @app.route('/home', methods=['POST', 'GET'])
 def home():
     Empresa = 'Lembrei'
-    data = request.form.get('data')    
+    data = request.form.get('data')
     hora = request.form.get('hora')
     email = request.form.get('email')
     phone = request.form.get('phone')
     message = request.form.get('message')
     if data != None:
-        insert=(iduser,data+" "+hora+":00",email,phone,message)
+        insert = (iduser, data + " " + hora + ":00", email, phone, message)
         bd.insert_msg(insert)
     print(iduser)
     cad_msg = bd.select_msg(iduser)
     flash(cad_msg)
-    return render_template("home.html", empresa=Empresa ,cad_msg = cad_msg )
+    return render_template("home.html", empresa=Empresa, cad_msg=cad_msg)
+
 
 @app.route('/contatos', methods=['POST', 'GET'])
 def contatos():
     Empresa = 'Lembrei'
-    data = request.form.get('data')    
+    data = request.form.get('data')
     hora = request.form.get('hora')
     email = request.form.get('email')
     phone = request.form.get('phone')
     message = request.form.get('message')
     if data != None:
-        insert=(iduser,data+" "+hora+":00",email,phone,message)
+        insert = (iduser, data + " " + hora + ":00", email, phone, message)
         bd.insert_msg(insert)
     print(iduser)
     cad_msg = bd.select_msg(iduser)
     flash(cad_msg)
-    return render_template("contatos.html", empresa=Empresa ,cad_msg = cad_msg )
+    return render_template("contatos.html", empresa=Empresa, cad_msg=cad_msg)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
